c67f33332d37be78c20102e8fd3dcdc903ffb78e
Teddy Bradsher <teddybradsher@gmail.com>
2020-05-22 15:47:00 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,10 @@
 # code your #valid_move? method here
-
+def valid_move?(board, index)
+if index[position] == filled or index[position] == invalid 
+  return false
+elsif index[position] == empty
+return true
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
