db4fbc0f7a35d3ead04702c40c6f3568dcedb4b9
Teddy Bradsher <teddybradsher@gmail.com>
2020-05-22 16:32:59 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,6 +4,7 @@ if index[position] == "X" or index[position] == "O" or index[position] = nil
   return false
 elsif index[position] == "" or index[position] == " "
 return true
+  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
