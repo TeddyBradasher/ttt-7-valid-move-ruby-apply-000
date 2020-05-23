cb47f7a48e00c01d55ae02298fcd398642ebf309
Teddy Bradsher <teddybradsher@gmail.com>
2020-05-23 01:50:11 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,20 +6,16 @@ def valid_move?(board, index)
       return true
     end
   end
-
-  def on_board?(num)
-    if num.between?(0, 8) == true
+  def on_board?(move)
+    if move.between?(0, 8) == true
       return true
     else
       return false
     end
   end
-
   if (position_taken?(board, index)) == false and (on_board?(index) == true)
     return true
   else
     return false
   end
-
-
 end
